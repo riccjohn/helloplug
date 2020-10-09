5ab25e594bea97d71860350486f1ad59614d53b9
riccjohn <riccjohn@gmail.com>
2020-10-09 16:54:16 -0500

    Replaces manual calls with Plug.Router

--- lib/routers/website_router.ex
+++ lib/routers/website_router.ex
@@ -1,29 +1,12 @@
 defmodule WebsiteRouter do
-  def init(default_opts) do
-    default_opts
-  end
+  use Plug.Router
 
-  def call(conn, _opts) do
-    route(conn.method, conn.path_info, conn)
-  end
+  plug :match
+  plug :dispatch
 
-  @user_router_options UserRouter.init([])
-  def route("GET", ["users" | _path], conn) do
-    UserRouter.call(conn, @user_router_options)
-  end
+  forward "/users", to: UserRouter
 
-  def route("GET", ["hello"], conn) do
-    conn
-    |> Plug.Conn.send_resp(200, "Hello, world!")
-  end
-
-  def route("GET", ["hello", name], conn) do
-    conn
-    |> Plug.Conn.send_resp(200, "Hello, #{name}!")
-  end
-
-  def route(_method, _path, conn) do
-    # this route is called if no other routes match
-    conn |> Plug.Conn.send_resp(404, "Couldn't find that page, sorry!")
+  match _ do
+    send_resp(conn, 404, "Couldn't find that page, sorry!")
   end
 end
